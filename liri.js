7798236b8733d0b9b6bdce5f006451283757a65b
danielpv <danielpvk@gmail.com>
2018-10-13 20:15:29 -0500

    first version

---
+++ liri.js
@@ -0,0 +1,47 @@
+//var dotenv=require("dotenv").config();
+var Spotify = require('node-spotify-api');
+//var spotify = new Spotify(keys.spotify);
+// Load the fs package to read and write
+var fs = require("fs");
+// Take two arguments.
+// The first will be the action (i.e. "concert-this`* `spotify-this-song`* `movie-this`* `do-what-it-says`
+// The second will the search term
+var [action,...extra]=process.argv.slice(2);//action guardamos lo de 2, y en extra todo lo demas
+var value=extra.join(" ");
+// We will then create a switch-case statement (if-else would also work).
+// The switch-case will direct which function gets run.
+switch (action) {
+    case "concert-this":
+        
+            break;
+    case "spotify-this-song":spoti(value);
+            console.log("funciona");
+            break;
+    case "movie-this-song":
+
+             break;
+    case "do-what-it-says":
+    
+            break;
+}
+
+function spoti(song){
+ 
+ 
+    var spotify = new Spotify({
+        id: "5b0c9495395746a2964d3373501d3990",
+        secret: "8e3925c8235d43718d91d96a609f8e38"
+    });
+    console.log(value);
+    spotify.search({ type: 'track', query: song }, function(err, data) {
+        if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+ 
+console.log(data); 
+console.log(data.tracks.items[0]); 
+});
+}
+
+console.log("**************");
+console.log("APP NOT READY");
